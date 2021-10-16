0bd7917a2928c01a53efd0d46bff58b9cd89615a
Daniela Escobar <daniela.b.escobar@gmail.com>
2021-10-16 18:42:52 +0200

    Fixed Dictionary and added more materials

--- Fabriken/InMaterials.cs
+++ Fabriken/InMaterials.cs
@@ -12,28 +12,66 @@ namespace Fabriken
         {
             Dictionary<string, int> materials = new Dictionary<string, int>
             {
-                {"Metal", 0 },
-                {"Rubber", 0 },
-                {"Wood", 0 }
+                //{"Metal", 0 },
+                //{"Rubber", 0 },
+                //{"Wood", 0 }
             };
-
-            Console.WriteLine("Choose material to add: ");
-            string materialOption = Console.ReadLine();
-            if (materialOption == "Metal")
+            bool loop = true;
+            while (loop)
             {
-                Console.WriteLine("How many metal/s do you wanna add?");
-                int numberOf = Convert.ToInt32(Console.ReadLine());
-
-                materials.CurrentInventory("Metal", numberOf);
-
-                foreach (var (key, value) in materials)
+                Console.Clear();
+                Console.WriteLine("Choose material to add: ");
+                string materialOption = Console.ReadLine();
+                if (materialOption == "Metal" || materialOption == "metal")
                 {
-                    Console.WriteLine(key + " : " + value);
+                    Console.WriteLine("How many units of metal do you wanna add?");
+                    int numberOf = Convert.ToInt32(Console.ReadLine());
+
+                    materials.Add("Metal", numberOf);
+
+                    foreach (var (key, value) in materials)
+                    {
+                        Console.WriteLine(key + " : " + value);
+                    }
+                    Console.ReadLine();
+                    loop = false;
+                }
+                else if (materialOption == "Rubber" || materialOption == "rubber")
+                {
+                    Console.WriteLine("How many units of rubber do you wanna add?");
+                    int numberOf = Convert.ToInt32(Console.ReadLine());
+
+                    materials.Add("Rubber", numberOf);
+
+                    foreach (var (key, value) in materials)
+                    {
+                        Console.WriteLine(key + " : " + value);
+                    }
+                    Console.ReadLine();
+                    loop = false;
+                }
+                else if (materialOption == "Wood" || materialOption == "wood")
+                {
+                    Console.WriteLine("How many units of wood do you wanna add?");
+                    int numberOf = Convert.ToInt32(Console.ReadLine());
+
+                    materials.Add("Wood", numberOf);
+
+                    foreach (var (key, value) in materials)
+                    {
+                        Console.WriteLine(key + " : " + value);
+                    }
+                    Console.ReadLine();
+                    loop = false;
+                }
+                else
+                {
+                    Console.WriteLine("Didn't recognize command, be sure to write one of following options");
+                    Console.WriteLine("Metal\nRubber\nWood\nPress any key to continue");
+                    Console.ReadLine();
+                    continue;
                 }
             }
-
-
-
         }
     }
 }
